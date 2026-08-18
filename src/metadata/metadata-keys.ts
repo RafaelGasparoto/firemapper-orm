f438bcf1fd8c433cd994f6e4edac31dd11423a2c
RafaelGasparoto <rafaelgasparoto4@gmail.com>
2026-08-17 21:06:18 -0300

    Constantes chaves de metadados

---
+++ src/metadata/metadata-keys.ts
@@ -0,0 +1,3 @@
+export const ENTITY_METADATA_KEY = Symbol("firemapper:entity");
+export const COLUMN_METADATA_KEY = Symbol("firemapper:columns");
+export const RELATION_METADATA_KEY = Symbol("firemapper:relations");
